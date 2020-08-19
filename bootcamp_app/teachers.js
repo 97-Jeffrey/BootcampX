bce8c91621ef9469d3963175ee01231f9b27a129
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-08-19 22:18:35 +0000

    new file to query teacher name and cohort name

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers JOIN assistance_requests ON teachers.id = teacher_id 
+JOIN students ON student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teachers.name;
+`)
+.then(res=>{
+  res.rows.forEach(each=>{
+    console.log(`${each.cohort}: ${each.teacher}`)
+  })
+
+})
+.catch(err => console.error('Query error:', err.stack));
